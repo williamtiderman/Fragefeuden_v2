e28b5cfee41625269d6b85bd7ad6475bcf2f7d7f
John <engblomsandinjohn@gmail.com>
2021-01-02 14:00:50 +0100

    GameplayActivity  // john

--- app/src/main/java/com/ete/fragefeudenv2/gamesActivity.java
+++ app/src/main/java/com/ete/fragefeudenv2/gamesActivity.java
@@ -67,9 +67,11 @@ public class gamesActivity extends AppCompatActivity {
         joinGameButton.setEnabled(false);
         createNewGameButton.setText("Laddar Aktiva Spel...");
 
+        Toast.makeText(gamesActivity.this, playerName, Toast.LENGTH_LONG).show();
+
         //Hämtar spelarens aktiva spel från databasen
 
-        myRef = FirebaseDatabase.getInstance().getReference().child("players").child("William");
+        myRef = FirebaseDatabase.getInstance().getReference().child("players").child(playerName);
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
